a862c675b4fb238183fc00ac931f98ee5be0c247
Rodrigo Ferrari <rodrigoferrari94rf@gmail.com>
2025-05-13 18:20:54 -0300

    update site

--- docs/main.js
+++ docs/main.js
@@ -97,7 +97,7 @@ if (cashNegativeTotalElement) {
   cashNegativeTotalElement.textContent = cashSumNegativeFormatado;
 }
 
-const cashTotalManual = "R$-430,61";
+const cashTotalManual = "R$-330,61";
 
 cashTotalElements.forEach((element) => {
   element.textContent = cashTotalManual;
